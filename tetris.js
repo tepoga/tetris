87d93be62c17d77edd41cf4751d9a9450cdda644
Teppei Ogawa <refills.squat.0r@icloud.com>
2023-11-07 00:36:05 +0900

    テトリミノを表示させ、それを操作できるようにしている途中（不具合発生中）

--- tetris.js
+++ tetris.js
@@ -18,6 +18,20 @@ canvas.height = canvasH;
 const container = document.getElementById("container")
 container.style.width = canvasW + 'px';
 
+// テトリミノ1片の大きさ
+const tetSize = 4;
+//T字型のテトリミノ
+let tet = [
+    [0, 0, 0, 0],
+    [0, 1, 0, 0],
+    [1, 1, 1, 0],
+    [0, 0, 0, 0],
+]
+
+//テトリミノのオフセット量（何マス分ずれているか）
+let offsetX = 0;
+let offsetY = 0;
+
 const draw = () => {
     SecondContext.fillStyle = '#000';
     SecondContext.fillRect(0, 0, canvasW, canvasH);
@@ -26,6 +40,36 @@ const draw = () => {
     SecondContext.fillStyle = "#f00";
     //x座標150,y座標150の場所に幅30,縦30の四角を描画
     SecondContext.fillRect(150, 150, blockSize, blockSize);
+
+    //テトリミノの描画
+    for (let y = 0; y < tetSize; y++) {
+        for (let x = 0; x < tetSize; x++) {
+            if (tet[y][x]) {
+                SecondContext.fillRect(
+                    (offsetX + x) * blockSize,
+                    (offsetY + y) * blockSize,
+                    blockSize,
+                    blockSize);
+            }
+        }
+    }
+}
+document.onkeydown = (e) => {
+    switch (e.keyCode) {
+        case 37: //左
+            offsetX--;
+            break;
+        case 38: //上
+            offsetY--;
+            break;
+        case 39: //右
+            offsetX++;
+            break;
+        case 40: //下
+            offsetY++;
+            break;
+
+    }
 }
 //初期化処理
 const init = () => {
