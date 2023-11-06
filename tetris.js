f0ee0315f3a00b5da607192e52619f379e29e4f9
Teppei Ogawa <refills.squat.0r@icloud.com>
2023-11-06 19:49:17 +0900

    初版

---
+++ tetris.js
@@ -0,0 +1,29 @@
+
+//ボードのサイズ
+const blockSize = 30;
+const boardRow = 20;
+const boardCol = 10;
+
+
+const canvas = document.getElementById("canvas")
+// 2ndコンテキストを取得
+const SecondContext = canvas.getContext("2d")
+//キャンバスサイズ
+const canvasW = blockSize * boardCol;
+const canvasH = blockSize * boardRow;
+canvas.width = canvasW;
+canvas.height = canvasH;
+
+//
+const container = document.getElementById("container")
+container.style.width = canvasW + 'px';
+
+const draw=()=> {
+    SecondContext.fillstyle = '#000';
+    SecondContext.fillRect(0, 0, canvasW, canvasH);
+}
+//初期化処理
+const init =()=>{
+    draw();
+
+}
